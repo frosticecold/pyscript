18281514b2cc049445019e7e73e6dd5c9dec581f
Raúl Correia <raulcorreia7@gmail.com>
2016-09-05 17:55:20 +0100

    More work done

--- lunch.py
+++ lunch.py
@@ -1,18 +1,17 @@
-#from openpyxl import load_workbook
-from __future__ import print_function
-#import warnings
-#~ warnings.filterwarnings("ignore")
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from openpyxl import load_workbook
+import easygui
+import warnings
+import os
+import datetime
 
-#~ ficheiroFonte = ""
 
-#Files with source data
-#~ wbFonte = load_workbook('F:\\Work\\Ementas\\ementa7_20_07_2016.xlsx')
-#~ wsFonte = wbFonte.worksheets[0]
+#from __future__ import print_function
 
-#~ #Target Files
-#~ wbTarget = load_workbook('F:\\Work\\Ementas\\teste.xlsx')
-#~ wsTarget = wbTarget.worksheets[0]
-
+#Ignore warnings
+warnings.filterwarnings("ignore")
+#----------------------VARIABLES----------------------------
 #MODDED FONTE IN A list of lists
 salasFonte = [[5,6,8,9,10],
 	[12,13,14,15,16,17],
@@ -20,65 +19,82 @@ salasFonte = [[5,6,8,9,10],
 	[26,27,28,29,30,31],
 	[33,34,35,36]]
 letrasF = 	['B','E','H','K','N']
+#salas = 	[s1BerF,s2BerF,sParqueF,sPreCatlF,dietaF]
 	
-#FONTE
-#s1BerF = 	
-#s2BerF = 	
-#sParqueF = 	
-#sPreCatlF = 
-#dietaF = 	
-
-
 
 #TARGET
-salasTarget = [[6,7,9,10,11],
-	[15,16,17,18,19,20],
-	[24,25,26,27,28,29],
-	[33,34,35,36,37,38],
-	[42,43,44,45]]
+ementaNrTgt = "C2"
+salasTarget = [[6,7,9,10,11], 
+	[15,16,17,18,19,20],	
+	[24,25,26,27,28,29],	
+	[33,34,35,36,37,38],	
+	[42,43,44,45]]			
 letrasT = 	['B','C','D','E','F']
 
-#~ s1BerT = 	
-#~ s2BerT = 	
-#~ sParqueT = 	
-#~ sPreCatlT =     
-#~ dietaT = 	
-#salas = 	[s1BerT,s2Ber,sParque,sPreCatl,dieta]
+posSegunda = "J3"
+posSexta = "J4"
+#salas = 	[s1BerT,s2BerT,sParqueT,sPreCatlT,dietaT]
 
-#LOGOTIPO
-#logo = "F:\\Raul\Work\Ementas\Logotipo_alta_png.png"
+#format dia
+fExl ="%d-%m-%Y"
+fFile ="%d_%m_%Y"
+#Nr Ementa
+ementaNr = easygui.integerbox(msg="Qual é o número da ementa?",
+title ="Ementa",default=0,lowerbound=0,upperbound=10)
+
+#Dia de hoje
+hoje = datetime.date.today()
+hojeF = hoje.strftime("%d_%m_%Y")
+proxSeg = 0
+proxSexta = 0
+#----------------END OF VARIABLES------------------
 
 
-for lF in range(len(letrasF)):
-	print("\n	",letrasF[lF])
-	for nrS in range(len(salasFonte)):
-		print("\n Sala: ",nrS)
-		for nrL in range(len(salasFonte[nrS])):
-			print(nrL,end=' ')
+#Ficheiro fonte
+ficheiroFonte = easygui.fileopenbox()
+
+#Ficheiro a gravar
+ficheiroTarget = os.path.join(os.getcwd(), "modelo.xlsx")
+
+if ficheiroFonte != None and ficheiroTarget != False :
+	#Workbook source
+	wbFonte = load_workbook(ficheiroFonte)
+	wsFonte = wbFonte.worksheets[0]
+
+	#Workbook target
+	wbTarget = load_workbook(ficheiroTarget)
+	wsTarget = wbTarget.worksheets[0]
 
 
-#~ #SALA BER1
-#~ for i in range(len(letrasF)):
-	#~ for k in range(len(s1BerF)):
-			#~ wsTarget["%s%d" % (letrasT[i],s1BerT[k])].value = wsFonte["%s%d" % (letrasF[i],s1BerF[k])].value
-				
-#~ #SALA BER2
-#~ for i in range(len(letrasF)):
-	#~ for k in range(len(s2BerF)):
-			#~ wsTarget["%s%d" % (letrasT[i],s2BerT[k])].value = wsFonte["%s%d" % (letrasF[i],s2BerF[k])].value
+#Este método calcula a próxima segunda e sexta feira
+def calcularDia():
+	global proxSeg
+	global proxSexta
+	if datetime.date.today() != None:
+		x = datetime.date.today()
+		while 1:
+			if datetime.date.today().weekday() == 0:
+				x =  datetime.date.today() + datetime.timedelta(1)
+			else:
+				x += datetime.timedelta(1)
+			if x.weekday() == 0:
+				proxSeg = x
+				proxSexta = x + datetime.timedelta(4)
+				break
+	return
 
-#~ #SALA PARQUE
-#~ for i in range(len(letrasF)):
-	#~ for k in range(len(sParqueF)):
-			#~ wsTarget["%s%d" % (letrasT[i],sParqueT[k])].value = wsFonte["%s%d" % (letrasF[i],sParqueF[k])].value
+#Este método copia dados um ficheiro de excel para outro
+def copiarDadosExl():
+	for lF in range(len(letrasF)):
+		for nrS in range(len(salasFonte)):
+				for nrL in range(len(salasFonte[nrS])):
+					wsTarget["%s%d" % (letrasT[lF],salasTarget[nrS][nrL])].value = wsFonte["%s%d" % (letrasF[lF],salasFonte[nrS][nrL])].value
 
-#~ #PRE/CATL
-#~ for i in range(len(letrasF)):
-	#~ for k in range(len(sPreCatlF)):
-			#~ wsTarget["%s%d" % (letrasT[i],sPreCatlT[k])].value = wsFonte["%s%d" % (letrasF[i],sPreCatlF[k])].value
-#~ #DIETA
-#~ for i in range(len(letrasF)):
-	#~ for k in range(len(dietaF)):
-			#~ wsTarget["%s%d" % (letrasT[i],dietaT[k])].value = wsFonte["%s%d" % (letrasF[i],dietaF[k])].value
-		
-#wbTarget.save('teste7_20_07_2016.xlsx')
+
+calcularDia()
+copiarDadosExl()
+
+wsTarget[posSegunda].value = proxSeg.strftime(fExl)
+wsTarget[posSexta].value = proxSexta.strftime(fExl)
+wsTarget[ementaNrTgt].value = ementaNr
+wbTarget.save("%s_%s_%s%s" %("resultado",ementaNr,hojeF,".xlsx"))
